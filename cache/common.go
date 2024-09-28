f1e7f04a4dfc5f2d2cc9048d5072c93731613e4b
Mikhail Knyazhev <markus621@yandex.com>
2024-09-29 01:48:46 +0300

    add ttl to cache

--- cache/common.go
+++ cache/common.go
@@ -1,9 +1,12 @@
 package cache
 
+import "time"
+
 type TCache[K comparable, T interface{}] interface {
 	Has(key K) bool
 	Get(key K) (T, bool)
 	Set(key K, value T)
+	SetWithTTL(key K, value T, ttl time.Time)
 	Del(key K)
 	Keys() []K
 	Flush()
